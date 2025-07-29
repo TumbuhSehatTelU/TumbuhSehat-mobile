95e99a4c2465fa35fc7da81df6c13f596b46f9ba
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-07-29 11:36:26 +0700

    chore: set up configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_tumbuh_sehat/app/presentation/pages/main_page.dart';
+// import 'package:mobile_tumbuh_sehat/app/presentation/pages/main_page.dart';
 import 'package:mobile_tumbuh_sehat/features/auth/presentation/pages/splash_screen.dart';
-import 'package:mobile_tumbuh_sehat/features/nutrition_prediction/presentation/pages/analisis_gizi_scan.dart';
-import 'package:mobile_tumbuh_sehat/features/nutrition_prediction/presentation/pages/scan_makanan.dart';
+// import 'package:mobile_tumbuh_sehat/features/nutrition_prediction/presentation/pages/analisis_gizi_scan.dart';
+// import 'package:mobile_tumbuh_sehat/features/nutrition_prediction/presentation/pages/scan_makanan.dart';
+import 'app/core/di/service_locator.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await di.init();
+
   runApp(const MainApp());
 }
 
