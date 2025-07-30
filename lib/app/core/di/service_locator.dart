8c8efc91e7bec2a3e31923ea4f2f44e3cea1e309
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-07-31 00:22:07 +0700

    feat(network): add service to check network

--- lib/app/core/di/service_locator.dart
+++ lib/app/core/di/service_locator.dart
@@ -1,7 +1,12 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mobile_tumbuh_sehat/app/core/network/network_info.dart';
+import 'package:mobile_tumbuh_sehat/app/core/network/network_info_impl.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
 
+  sl.registerLazySingleton(() => Connectivity());
 }
---
+++ lib/app/core/network/network_info_impl.dart
@@ -0,0 +1,17 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'network_info.dart';
+
+class NetworkInfoImpl implements NetworkInfo {
+  final Connectivity _connectivity;
+
+  NetworkInfoImpl(this._connectivity);
+
+  @override
+  Future<bool> get isConnected async {
+    final result = await _connectivity.checkConnectivity();
+    if (result == ConnectivityResult.none) {
+      return false;
+    }
+    return true;
+  }
+}
