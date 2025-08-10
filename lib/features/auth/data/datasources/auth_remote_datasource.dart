e3836686b15b994d24cd2bc8dcbc8a333941761b
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-08-11 00:04:35 +0700

    feat(auth module): add auth data remote resource

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -2,20 +2,29 @@ import 'package:mobile_tumbuh_sehat/features/auth/data/models/family_model.dart'
 import 'package:mobile_tumbuh_sehat/features/auth/data/models/parent_model.dart';
 
 abstract class AuthRemoteDataSource {
+  // POST /auth/login
   Future<FamilyModel> login({
     required String phone,
     required String name,
     required String password,
   });
 
+  // POST /auth/register
   Future<void> registerNewFamily({
     required FamilyModel family,
     required String password,
   });
 
+  // POST /family/{familyId}/join
   Future<void> joinExistingFamily({
     required String familyId,
     required ParentModel newParent,
     required String password,
   });
+
+  // GET /family/{familyId}
+  Future<FamilyModel> getFamily(String familyId);
+
+  // POST /auth/logout
+  Future<void> logout();
 }
--- lib/features/auth/data/datasources/auth_remote_datasource_impl.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource_impl.dart
@@ -5,6 +5,7 @@ import 'package:mobile_tumbuh_sehat/app/core/config/app_config.dart';
 import 'package:mobile_tumbuh_sehat/app/core/error/exceptions.dart';
 import 'package:mobile_tumbuh_sehat/features/auth/data/datasources/auth_remote_datasource.dart';
 import 'package:mobile_tumbuh_sehat/features/auth/data/models/family_model.dart';
+import 'package:mobile_tumbuh_sehat/features/auth/data/models/parent_model.dart';
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   final Dio dio;
@@ -14,16 +15,16 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   @override
   Future<FamilyModel> login({
     required String phone,
+    required String name,
     required String password,
   }) async {
-    if (AppConfig.baseUrl.isEmpty) {
-      throw ServerException();
-    }
+    if (AppConfig.baseUrl.isEmpty) throw ServerException();
 
     try {
       final response = await dio.post(
+        // POSTPONE ENDPOINT
         '${AppConfig.baseUrl}/auth/login',
-        data: {'phone': phone, 'password': password},
+        data: {'phone': phone, 'name': name, 'password': password},
       );
 
       if (response.statusCode == 200) {
@@ -37,18 +38,16 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   }
 
   @override
-  Future<void> register({
+  Future<void> registerNewFamily({
     required FamilyModel family,
     required String password,
   }) async {
-    if (AppConfig.baseUrl.isEmpty) {
-      throw ServerException();
-    }
+    if (AppConfig.baseUrl.isEmpty) throw ServerException();
 
     try {
       final requestData = family.toJson()..addAll({'password': password});
-
       final response = await dio.post(
+        // POSTPONE ENDPOINT
         '${AppConfig.baseUrl}/auth/register',
         data: requestData,
       );
@@ -58,6 +57,61 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
       }
     } on DioException {
       throw ServerException();
+    }
+  }
+
+  @override
+  Future<void> joinExistingFamily({
+    required String familyId,
+    required ParentModel newParent,
+    required String password,
+  }) async {
+    if (AppConfig.baseUrl.isEmpty) throw ServerException();
+
+    try {
+      final requestData = newParent.toJson()..addAll({'password': password});
+      final response = await dio.post(
+        // POSTPONE ENDPOINT
+        '${AppConfig.baseUrl}/family/$familyId/join',
+        data: requestData,
+      );
+
+      if (response.statusCode != 200) {
+        throw ServerException();
+      }
+    } on DioException {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<FamilyModel> getFamily(String familyId) async {
+    if (AppConfig.baseUrl.isEmpty) throw ServerException();
+
+    try {
+      // POSTPONE ENDPOINT
+      final response = await dio.get('${AppConfig.baseUrl}/family/$familyId');
+      if (response.statusCode == 200) {
+        return FamilyModel.fromJson(response.data['data']);
+      } else {
+        throw ServerException();
+      }
+    } on DioException {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<void> logout() async {
+    if (AppConfig.baseUrl.isEmpty) {
+      return;
+    }
+
+    try {
+      await dio.post('${AppConfig.baseUrl}/auth/logout');
+    // ignore: empty_catches
+    } on DioException {
+      
     }
   }
 }
